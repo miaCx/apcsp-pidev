6b8ed847f9eb12b5be2bc97608548c0ecb5d4b42
Mia <chyungm0051@my.sduhsd.net>
2019-12-17 12:07:24 -0800

    variables.c

---
+++ variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  int b = 3;
+  int c;
+
+  int d;
+
+  a = 2;
+  c = a + b;
+  printf("Sum of %d and %d is %d\n", a, b, c);
+  
+  d = a;
+  a = b;
+  b = d;
+  printf("Sum of %d and %d is %d\n", a, b, c);
+}
