56c640ab9828f76bea66a14d9ab8a11fddc1c0d2
Mia <chyungm0051@my.sduhsd.net>
2020-01-06 12:20:28 -0800

    pointers

---
+++ pointers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+  
+  int a;
+  int* ptrtoa;
+
+  ptrtoa = &a;
+
+  a = 5;
+  printf("The value of a is %d\n", a);
+
+  *ptrtoa = 6;
+  printf("The value of a is %d\n", a);
+
+  printf("The value of ptrtoa is %d\n", ptrtoa);
+  printf("It stores the value %d\n", *ptrtoa);
+  printf("The address of a is %d\n", &a);
+
+  float d = 5;
+  float e = 6;
+  int* ptrtod;
+  int* ptrtoe;
+
+
+  printf("d is %f\ne is %f\n", d, e);
+  printf("address of d is %d\naddress of e is %d\n", &d, &e);
+}
