c7d79aa796eab6bf4b18d7089782cd4798b87095
Mia <chyungm0051@my.sduhsd.net>
2020-01-06 13:08:09 -0800

    area of circle

---
+++ areaOfCircle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+float findA(float radius) {
+  float area = 3.141592653589793238462643383279502 * radius * radius;
+
+  return area;
+}
+
+int main()
+{
+  float area;
+  float i = 3.5;
+  while(i < 13) {
+    area = findA(i);
+    printf("The area is %f\n",area);
+    i++;
+  }
+}
