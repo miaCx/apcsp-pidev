0602a59cbc50c7c381e496325da6463b6ea1d7e9
Mia <chyungm0051@my.sduhsd.net>
2020-01-08 13:08:57 -0800

    areaofcircle2.c with range

--- areaofcircle2.c
+++ areaofcircle2.c
@@ -10,19 +10,85 @@ int main()
 {
   float area;
 
-  char input[256], name[256];
-  int radius;
+  char input[256];
+  float radius;
+  float outerradius;
 
-  printf("What is the radius?\n");
+  printf("What is the minimum radius?\n");
   while (1)
   {
     fgets(input, 256, stdin);
-    if (sscanf(input, "%d", &radius) == 1) break;
+    if (sscanf(input, "%f", &radius) == 1) break;
+    printf("Please enter a valid number.\n");
+  }
+
+  printf("What is the maximum radius?\n");
+  while (1)
+  {
+    fgets(input, 256, stdin);
+    if (sscanf(input, "%f", &outerradius) == 1) break;
     printf("Please enter a valid number.\n");
   }
 
   area = findA(radius);
-  printf("The area is %f\n",area);
+
+  float i = radius;
+  while(i < outerradius) {
+    area = findA(i);
+    printf("The radius is %f\n",i);
+    printf("The area is %f\n",area);
+    i++;
+  }
+//  printf("The area is %f\n",area);
+
 
 }
 
+/*
+#include <stdio.h>
+
+int main()
+{
+  char input[256], name[256];
+  int age;
+  
+  printf("What is your name?\n");
+  fgets(input, 256, stdin);
+  sscanf(input, "%s", name);
+
+  printf("Hello %s. How old are you?\n", name);
+  while (1)
+  {
+    fgets(input, 256, stdin);
+    if (sscanf(input, "%d", &age) == 1) break;
+    printf("Not a valid age - try again!\n");
+  }
+  
+  printf("Hi %s, you look young for %d…\n", name, age);
+}
+
+///////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+float findA(float radius) {
+  float area = 3.141592653589793238462643383279502 * radius * radius;
+
+  return area;
+}
+
+int main()
+{
+  float area;
+  float i = 3.5;
+  while(i < 13) {
+    area = findA(i);
+    printf("The radius is %f\n",i);
+    printf("The area is %f\n",area);
+    i++;
+  }
+}
+
+
+*/
