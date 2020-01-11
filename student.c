778c34a254b1de2611b0792f38ac5406be83809e
miaCx <33041196+miaCx@users.noreply.github.com>
2020-01-10 19:48:04 -0800

    Update student.c

--- student.c
+++ student.c
@@ -37,8 +37,8 @@ int main()
   printStudent(&student1);
 
   //adding a student
-  int adding = 1;
-  while (adding == 1) {
+  //int adding = 1;
+  while (1) {
     struct studenti;
     numberofstudents = numberofstudents + 1;
 
@@ -50,7 +50,7 @@ int main()
     printf("Enter a last name:\n");
     fgets(input, 256, stdin);
     sscanf(input, "%s", tmp);
-    strcpy(student1.firstname, tmp);
+    strcpy(student1.lastname, tmp);
 
     printf("Enter an age:\n");
     while (1) {
@@ -58,20 +58,60 @@ int main()
       if (sscanf(input, "%d", tmp) == 1) break;
       printf("Not a valid age.\n");
     }
+    student1.age = tmp;
 
     printf("Enter an id:\n");
     while (1) {
       fgets(input, 256, stdin);
       if (sscanf(input, "%d", tmp) == 1) break;
       printf("Not a valid id.\n");
-    adding = 0;
+    //adding = 0;
     }
+    //debugging
+    //printf("%s\n",tmp);
+
+
+    printf("Would you like to add another student? 1 for yes, 0 for no\n");
+    fgets(input, 256, stdin);
+    sscanf(input, "%d", tmp);
+ 
+    //debugging
+    //printf("%s\n",tmp);
+
+    if (*tmp == 0) {
+      break;
+    }
+    else if (*tmp != 1) {
+      while (1) {
+      printf("Please enter y or n.\n");
+      fgets(input, 256, stdin);
+      sscanf(input, "%d", tmp);
+      
+      //debugging
+      //printf("%s\n",tmp);
+      if (*tmp == 0) {
+      break;
+      }
+      else if (*tmp == 1) {
+      break;
+      }
+      
+      }
+      
+    }
+    /*
+    while (1) {
+      fgets(input, 256, stdin);
+      if (sscanf(input, "%s", tmp) == 1) break;
+      printf("Not a valid age.\n");
+    }
+    */
   }
 
   char studenttoprint[100];
   for (int i = i; i < numberofstudents; i++) {
     strcat("&student", i);
-    studenttoprint[i] = "&student1";
+    //studenttoprint[i] = "&student1";
   }
 
   for (int i = 1; i < numberofstudents; i++) {
@@ -84,11 +124,9 @@ int main()
 /* int main() {
   char input[256], name[256];
   int age;
-
   printf("What is your name?\n");
   fgets(input, 256, stdin);
   sscanf(input, "%s", name);
-
   printf("Hello %s. How old are you?\n", name);
   while (1)
   {
